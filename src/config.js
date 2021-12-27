dadc1ee3ce4c9f7e238e8441beb8eb955c6efa54
alphatheassistant <92854101+alphatheassistant@users.noreply.github.com>
2021-12-27 17:42:06 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	SIZE_LIMIT: 50, // mentioned in KB
 	PORT: process.env.PORT || 3000,
-	MONGO_URL: process.env.MONGODB_URI || "mongodb://localhost:27017/jsonbox-io-dev",
+	MONGO_URL: "mongodb+srv://api-point:satyanchal@cluster0.ey0ry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 	REQUEST_LIMIT_PER_HOUR: 100,
 	ENABLE_DATA_EXPIRY: false, // Once switched on the index will be be set in mongodb. Might need to remove it in order to switch off the behaviour
 	DATA_EXPIRY_IN_DAYS: 30,
